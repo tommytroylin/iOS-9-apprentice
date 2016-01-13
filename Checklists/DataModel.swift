fb60a79e0899be657b911cf7e58fdd0d9d8a02d7
Tommy Troy Lin <tommytroylin@gmail.com>
2016-01-13 18:40:06 +0800

    Rename Persist to DataModel

--- Checklists/DataModel.swift
+++ Checklists/DataModel.swift
@@ -1,24 +1,39 @@
 //
-//  Persist.swift
+//  DataModel.swift
 //  Checklists
 //
-//  Created by Keyu Lin on 1/30/16.
+//  Created by Keyu Lin on 1/12/16.
 //  Copyright © 2016 Keyu Lin. All rights reserved.
 //
 
 import Foundation
 
-class Persist {
-  class func documentsDirectory() -> String {
+class DataModel {
+  var checklists = [Checklist]()
+
+  init() {
+    load()
+  }
+  func save(){
+    saveObject(checklists,forkey:"checklists")
+  }
+
+  func load(){
+    if let checklistsFromFile = loadObjectByKey("checklists") as? [Checklist] {
+      checklists = checklistsFromFile
+    }
+  }
+
+  private func documentsDirectory() -> String {
     let paths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)
     return paths[0]
   }
 
-  class func dataFilePath() -> String {
+  private func dataFilePath() -> String {
     return (documentsDirectory() as NSString).stringByAppendingPathComponent("Checklists.plist")
   }
 
-  class func saveObject(object: Array<NSCoding>, forkey key: String) {
+  private func saveObject(object: Array<NSCoding>, forkey key: String) {
     let data = NSMutableData()
     let archiver = NSKeyedArchiver(forWritingWithMutableData: data)
     archiver.encodeObject(object, forKey: key)
@@ -26,18 +41,18 @@ class Persist {
     data.writeToFile(dataFilePath(), atomically: true)
   }
 
-  class func loadObjectByKey(key: String) -> AnyObject? {
+  private func loadObjectByKey(key: String) -> AnyObject? {
     let path = dataFilePath()
     if NSFileManager.defaultManager().fileExistsAtPath(path) {
       if let data = NSData(contentsOfFile: path) {
         let unarchiver = NSKeyedUnarchiver(forReadingWithData: data)
-        let objects = unarchiver.decodeObjectForKey(key)
+        let object = unarchiver.decodeObjectForKey(key)
         unarchiver.finishDecoding()
-        return objects
+        return object
       }
       return nil
     }
     return nil
   }
-
+  
 }
