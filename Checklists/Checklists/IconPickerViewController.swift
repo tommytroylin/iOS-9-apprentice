240926deb5b2a84fa3b4cda160baeaa2688635e9
Tommy Troy Lin <tommytroylin@gmail.com>
2016-01-30 19:00:48 +0800

    Refactor code

--- Checklists/Checklists/IconPickerViewController.swift
+++ Checklists/Checklists/IconPickerViewController.swift
@@ -9,19 +9,19 @@
 import UIKit
 
 protocol IconPickerViewControllerDelegate: class {
-  func iconPicker(picker: IconPickerViewController,didPickIcon iconName:String)
+  func iconPicker(picker: IconPickerViewController, didPickIcon iconName: String)
 }
 
 class IconPickerViewController: UITableViewController {
   weak var delegate: IconPickerViewControllerDelegate?
-  let icons = [ "No Icon", "Appointments", "Birthdays", "Chores", "Drinks", "Folder", "Groceries", "Inbox", "Photos", "Trips" ]
+  let icons = ["No Icon", "Appointments", "Birthdays", "Chores", "Drinks", "Folder", "Groceries", "Inbox", "Photos", "Trips"]
 
   override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     return icons.count
   }
 
   override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-    let cell = tableView.dequeueReusableCellWithIdentifier("IconCell",forIndexPath: indexPath)
+    let cell = tableView.dequeueReusableCellWithIdentifier("IconCell", forIndexPath: indexPath)
     let iconName = icons[indexPath.row]
     cell.textLabel!.text = iconName
     cell.imageView!.image = UIImage(named: iconName)
