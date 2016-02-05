698a6cd2bc75c2c18fa1ca152a7bce1ed4ff8acc
Tommy Troy Lin <tommytroylin@gmail.com>
2016-02-05 13:50:41 +0800

    Local notifications

--- Checklists/Checklists/AppDelegate.swift
+++ Checklists/Checklists/AppDelegate.swift
@@ -19,6 +19,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     let navigationController = window!.rootViewController as! UINavigationController
     let controller = navigationController.viewControllers[0] as! AllListsTableViewController
     controller.dataModel = dataModel
+    let notificationSettings = UIUserNotificationSettings(forTypes: [.Alert,.Sound], categories: nil)
+    UIApplication.sharedApplication().registerUserNotificationSettings(notificationSettings)
+    let date = NSDate(timeIntervalSinceNow: 10)
+    let localNotification = UILocalNotification()
+    localNotification.fireDate = date
+    localNotification.timeZone = NSTimeZone.defaultTimeZone()
+    localNotification.alertBody = "I am a local notification!"
+    localNotification.soundName = UILocalNotificationDefaultSoundName
+    UIApplication.sharedApplication().scheduleLocalNotification(localNotification)
     return true
   }
 
@@ -46,6 +55,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   func applicationWillTerminate(application: UIApplication) {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     dataModel.save()
+  }
+
+  func application(application: UIApplication, didReceiveLocalNotification notification: UILocalNotification) {
+    print("didReceiveLocalNotification \(notification)")
   }
 
 }
